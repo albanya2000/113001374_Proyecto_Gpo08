579084e9b0a4a770aeeaf2ea07d4b96590e9a600
albanyayala <ayat0708@hotmail.com>
2022-05-12 23:45:13 -0500

    12-05-22-fin2

--- ProyectoFinal/ProyectoFinal/113001374_ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/113001374_ProyectoFinal.cpp
@@ -32,6 +32,9 @@ bool firstMouse = true;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 float rot = 0.0f;
+float mov1 = 0.0f;
+float mov2 = 0.0f;
+float mov3 = 0.0f;
 
 int main( ){
     // Init GLFW
@@ -72,9 +75,17 @@ int main( ){
     Shader lampshader( "Shaders/lamp.vs", "Shaders/lamp.frag" );
 
     // Load models
+    //Alfombra
     Model alfombra1((char*)"Models/Alfombra/alfombra1.obj");
     Model alfombra2((char*)"Models/Alfombra/alfombra2.obj");
+    //Banco
     Model banco((char*)"Models/banco/banco.obj");
+    //Casa
+    Model casa((char*)"Models/casa/casaSinPuerta.obj");
+    Model puerta((char*)"Models/casa/puerta.obj");
+    //Chimenea
+    Model chimenea((char*)"Models/chimenea/chimenea.obj");
+    //Librero
     Model librero((char*)"Models/estante/Librero.obj");
     Model lObj1((char*)"Models/estante/Objeto1.obj");
     Model lObj2((char*)"Models/estante/Objeto2.obj");
@@ -88,7 +99,17 @@ int main( ){
     Model lObj10((char*)"Models/estante/Objeto10.obj");
     Model lObj11((char*)"Models/estante/Objeto11.obj");
     Model lObj12((char*)"Models/estante/Objeto12.obj");
-
+    //Fonógrafo
+    Model fonografo((char*)"Models/fonografo/fonografo.obj");
+    Model disco((char*)"Models/fonografo/disco.obj");
+    Model manivela((char*)"Models/fonografo/manivela.obj");
+    //Lampara
+    Model base((char*)"Models/lampara/base.obj");
+    Model mampara((char*)"Models/lampara/mampara.obj");
+    //Mesa
+    Model mesa((char*)"Models/mesa/mesa.obj");
+    //Sofa
+    Model sofa((char*)"Models/sofa/sofa.obj");
 
     glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
     GLfloat vertices[] = {
@@ -165,17 +186,124 @@ int main( ){
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
         // Draw the loaded model
-
         //Alfombra1
         glm::mat4 model(1);
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         alfombra1.Draw(shader);
         glBindVertexArray(0);
-
         //Alfombra2
         model = glm::mat4(1);
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         alfombra2.Draw(shader);
+        glBindVertexArray(0);
+        //Banco
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        banco.Draw(shader);
+        glBindVertexArray(0);
+        //Casa
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        casa.Draw(shader);
+        glBindVertexArray(0);
+        //puerta
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        puerta.Draw(shader);
+        glBindVertexArray(0);
+        //Chimenea
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        chimenea.Draw(shader);
+        glBindVertexArray(0);
+        //Estante
+        //Librero
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        librero.Draw(shader);
+        glBindVertexArray(0);
+        //Objetos
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj1.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj2.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj3.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj4.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj5.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj6.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj7.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj8.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj9.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj10.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj11.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        lObj12.Draw(shader);
+        glBindVertexArray(0);
+        //Fonógrafo
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        fonografo.Draw(shader);
+        glBindVertexArray(0);
+        //Disco
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        disco.Draw(shader);
+        glBindVertexArray(0);
+        //Manivela
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        manivela.Draw(shader);
+        glBindVertexArray(0);
+        //Lampara
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        base.Draw(shader);
+        glBindVertexArray(0);
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        mampara.Draw(shader);
+        glBindVertexArray(0);
+        //Mesa
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        mesa.Draw(shader);
+        glBindVertexArray(0);
+        //Sofa
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        sofa.Draw(shader);
         glBindVertexArray(0);
 
         //model = glm:: mat4(1);
@@ -195,78 +323,65 @@ int main( ){
 
 
 // Moves/alters the camera positions based on user input
-void DoMovement( )
-{
+void DoMovement( ){
     // Camera controls
-    if ( keys[GLFW_KEY_W] || keys[GLFW_KEY_UP] )
-    {
+    if ( keys[GLFW_KEY_W] || keys[GLFW_KEY_UP] ){
         camera.ProcessKeyboard( FORWARD, deltaTime );
     }
-    
-    if ( keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN] )
-    {
+    if ( keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN] ){
         camera.ProcessKeyboard( BACKWARD, deltaTime );
     }
-    
-    if ( keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT] )
-    {
+    if ( keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT] ){
         camera.ProcessKeyboard( LEFT, deltaTime );
     }
-    
-    if ( keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT] )
-    {
+    if ( keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT] ){
         camera.ProcessKeyboard( RIGHT, deltaTime );
     }
-
-    if (keys[GLFW_KEY_O])
-    {
-        rot += 0.5f;
-       
-
+    //Movimiento del sillon
+    if (keys[GLFW_KEY_1]){
+        mov1 += 0.5f;
     }
-
+    //Movimiento de la Lampara
+    if (keys[GLFW_KEY_2]) {
+        mov2 += 0.5f;
+    }
+    //Giro de Disco y Manivela
+    if (keys[GLFW_KEY_3]) {
+        mov3 += 0.5f;
+    }
+    if (keys[GLFW_KEY_4]) {
+        rot += 0.5f;
+    }
+    if (keys[GLFW_KEY_5]) {
+        rot += 0.5f;
+    }
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode )
-{
-    if ( GLFW_KEY_ESCAPE == key && GLFW_PRESS == action )
-    {
+void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode ){
+    if ( GLFW_KEY_ESCAPE == key && GLFW_PRESS == action ){
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
-    
-    if ( key >= 0 && key < 1024 )
-    {
-        if ( action == GLFW_PRESS )
-        {
+    if ( key >= 0 && key < 1024 ){
+        if ( action == GLFW_PRESS ){
             keys[key] = true;
         }
-        else if ( action == GLFW_RELEASE )
-        {
+        else if ( action == GLFW_RELEASE ){
             keys[key] = false;
         }
     }
-
- 
-
- 
 }
 
-void MouseCallback( GLFWwindow *window, double xPos, double yPos )
-{
-    if ( firstMouse )
-    {
+void MouseCallback( GLFWwindow *window, double xPos, double yPos ){
+    if ( firstMouse ){
         lastX = xPos;
         lastY = yPos;
         firstMouse = false;
     }
-    
     GLfloat xOffset = xPos - lastX;
     GLfloat yOffset = lastY - yPos;  // Reversed since y-coordinates go from bottom to left
-    
     lastX = xPos;
     lastY = yPos;
-    
     camera.ProcessMouseMovement( xOffset, yOffset );
 }
 
